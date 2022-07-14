530ee883965264d40a44e9da6529eeb1221652a0
tonnytang <96461132+TONNYTANGC@users.noreply.github.com>
2022-07-14 15:37:04 +0800

    Update TutorialRoom.java

--- BookingSystem/TutorialRoom.java
+++ BookingSystem/TutorialRoom.java
@@ -13,30 +13,5 @@ public class TutorialRoom extends Room{
     public TutorialRoom(String college, String venue, int level, int capacity, double price) {
         super(college, venue, level, capacity, price);
     }  
-
-    @Override
-    public void setPrice(double price) {
-        super.setPrice(price); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
-    @Override
-    public void setCapacity(int capacity) {
-        super.setCapacity(capacity); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
-    @Override
-    public void setLevel(int level) {
-        super.setLevel(level); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
-    @Override
-    public void setVenue(String venue) {
-        super.setVenue(venue); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
-    @Override
-    public void setCollege(String college) {
-        super.setCollege(college); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
     
 }
